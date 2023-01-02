03109b3cfc452391c51550d61e19909dd99f33fa
Prasetya Ikra Priyadi <prasetya.ikrapriyadi@gmail.com>
2023-01-02 10:53:52 +0700

    Preparing Database and test

---
+++ tests/AuthTableTestHelper.js
@@ -0,0 +1,28 @@
+/* istanbul ignore file */
+import pool from "@Infrastructures/database/postgres/pool";
+const AuthTableTestHelper = {
+  async addToken(token) {
+    const query = {
+      text: "INSERT INTO authentications VALUES($1)",
+      values: [token],
+    };
+
+    await pool.query(query);
+  },
+
+  async findToken(token) {
+    const query = {
+      text: "SELECT token FROM authentications WHERE token = $1",
+      values: [token],
+    };
+
+    const result = await pool.query(query);
+
+    return result.rows;
+  },
+  async cleanTable() {
+    await pool.query("TRUNCATE TABLE authentications");
+  },
+};
+
+export default AuthTableTestHelper;
